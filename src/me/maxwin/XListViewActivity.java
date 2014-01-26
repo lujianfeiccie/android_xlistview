dc176694f3411deaf4f9096a4c4ca3e4c94beaed
lujianfeiccie <lujianfeiccie@gmail.com>
2014-01-26 12:29:34 +0800

    first commit

---
+++ src/me/maxwin/XListViewActivity.java
@@ -0,0 +1,92 @@
+package me.maxwin;
+
+import java.util.ArrayList;
+
+import me.maxwin.view.XListView;
+import me.maxwin.view.XListView.IXListViewListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class XListViewActivity extends Activity implements IXListViewListener {
+	private XListView mListView;
+	private ArrayAdapter<String> mAdapter;
+	private ArrayList<String> items = new ArrayList<String>();
+	private Handler mHandler;
+	private int start = 0;
+	private String tag = "XListViewActivity";
+	private static int refreshCnt = 0;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		geneItems();
+		mListView = (XListView) findViewById(R.id.xListView);
+		mListView.setPullLoadEnable(true);
+		mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+		mListView.setAdapter(mAdapter);
+//		mListView.setPullLoadEnable(false);
+//		mListView.setPullRefreshEnable(false);
+		mListView.setXListViewListener(this);
+		mHandler = new Handler();
+		mListView.showRefreshing();
+		mListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Toast.makeText(getApplicationContext(), ""+arg2, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void geneItems() {
+		for (int i = 0; i != 9; ++i) {
+			items.add("refresh cnt " + (++start));
+		}
+	}
+
+	private void onLoad() {
+		mListView.stopRefresh();
+		mListView.stopLoadMore();
+		mListView.setRefreshTime("刚刚");
+	}
+	
+	@Override
+	public void onRefresh() {
+		log("onRefresh");
+		mHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				start = ++refreshCnt;
+				items.clear();
+				geneItems();
+				// mAdapter.notifyDataSetChanged();
+				mAdapter = new ArrayAdapter<String>(XListViewActivity.this, R.layout.list_item, items);
+				mListView.setAdapter(mAdapter);
+				onLoad();
+			}
+		}, 2000);
+	}
+
+	@Override
+	public void onLoadMore() {
+		mHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				geneItems();
+				mAdapter.notifyDataSetChanged();
+				onLoad();
+			}
+		}, 2000);
+	}
+	void log(String msg){
+		Log.d(tag , msg);
+	}
+}
